a27206d427d6cf8d619d13148a2d278da596770c
Oleg Andrushko <nubick@gmail.com>
2016-03-02 00:23:42 +0700

    New loop added: MoveLoop

---
+++ TweenPeaks/Assets/Plugins/TweenPeaks/Loops/MoveLoop.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Plugins.TweenPeaks.Tweens;
+using UnityEngine;
+
+namespace Assets.Plugins.TweenPeaks.Loops
+{
+    public class MoveLoop : Loop
+    {
+        private Vector3 _startPosition;
+
+        public Vector3 Offset;
+        public float Duration = 1f;
+        public Ease Ease;
+
+        public void Awake()
+        {
+            _startPosition = transform.position;
+        }
+
+        public void OnEnable()
+        {
+            RunPartOneMoveTween();
+        }
+
+        public void OnDisable()
+        {
+            MoveToTween moveToTween = GetComponent<MoveToTween>();
+            if (moveToTween != null)
+                Destroy(moveToTween);
+        }
+
+        private void RunPartOneMoveTween()
+        {
+            transform.position = _startPosition;
+            MoveToTween.Run(gameObject, _startPosition + Offset, Duration).SetEase(Ease).OnFinish(RunPartTwoMoveTween);
+        }
+
+        private void RunPartTwoMoveTween()
+        {
+            transform.position = _startPosition + Offset;
+            MoveToTween.Run(gameObject, _startPosition, Duration).SetEase(Ease).OnFinish(RunPartOneMoveTween);
+        }
+    }
+}
