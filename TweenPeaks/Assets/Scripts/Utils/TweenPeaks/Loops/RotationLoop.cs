8807cb7ff011363bb323856440ea4d31a183425f
Oleg Andrushko <nubick@gmail.com>
2017-12-05 11:52:52 +0700

    Loops: RotationLoop introduction.

---
+++ TweenPeaks/Assets/Scripts/Utils/TweenPeaks/Loops/RotationLoop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utils.TweenPeaks.Loops
+{
+    public class RotationLoop : MonoBehaviour
+    {
+        public float Speed = 45f;
+
+        public void Update()
+        {
+            transform.Rotate(0f, 0f, Speed * Time.deltaTime);
+        }
+    }
+}
