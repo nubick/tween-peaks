30b249482c9a4d719994bf7765d07e6b5b2c7e01
Oleg Andrushko <nubick@gmail.com>
2018-02-22 11:59:25 +0700

    BlinkLoop: UI graphics elements support. Renderer support. Elements with children support. Examples.

--- TweenPeaks/Assets/Scripts/Utils/TweenPeaks/Loops/BlinkLoop.cs
+++ TweenPeaks/Assets/Scripts/Utils/TweenPeaks/Loops/BlinkLoop.cs
@@ -1,33 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Utils.TweenPeaks.Loops
 {
     public class BlinkLoop : MonoBehaviour
     {
+        private Action _hideRootAction;
+        private Action _showRootAction;
+
+        private Graphic _graphic;
+        private Renderer _renderer;
+
         public float BlinkDelay;
+
+        public void Awake()
+        {
+            CacheElements();
+        }
+
+        private void CacheElements()
+        {
+            _graphic = GetComponent<Graphic>();
+            if (_graphic != null)
+            {
+                _hideRootAction = () => _graphic.enabled = false;
+                _showRootAction = () => _graphic.enabled = true;
+                return;
+            }
+
+            _renderer = GetComponent<Renderer>();
+            if(_renderer != null)
+            {
+                _hideRootAction = () => _renderer.enabled = false;
+                _showRootAction = () => _renderer.enabled = true;
+                return;
+            }
+
+            Debug.Log("BlinkLoop. Can't find any visual element to blink.");
+        }
 
         public void OnEnable()
         {
             StopAllCoroutines();
-            StartCoroutine(BlinkCoroutine());
+            if (_showRootAction != null)
+                StartCoroutine(BlinkCoroutine());
         }
 
         private IEnumerator BlinkCoroutine()
         {
             for (;;)
             {
-				//yield return new WaitForSeconds(BlinkDelay);
-				//GetComponent<Renderer>().enabled = false;
-				//yield return new WaitForSeconds(BlinkDelay);
-				//GetComponent<Renderer>().enabled = true;
-
-				//yield return new WaitForSeconds(BlinkDelay);
-				//renderer.enabled = false;
-				//yield return new WaitForSeconds(BlinkDelay);
-				//renderer.enabled = true;
+                Show();
+                yield return new WaitForSeconds(BlinkDelay);
+                Hide();
+                yield return new WaitForSeconds(BlinkDelay);
             }
         }
 
+        private void Show()
+        {
+            _showRootAction();
+            UpdateChildrenVisibility(true);
+        }
+
+        private void Hide()
+        {
+            _hideRootAction();
+            UpdateChildrenVisibility(false);
+        }
+
+        private void UpdateChildrenVisibility(bool isVisible)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+                transform.GetChild(i).gameObject.SetActive(isVisible);
+        }
     }
 }
