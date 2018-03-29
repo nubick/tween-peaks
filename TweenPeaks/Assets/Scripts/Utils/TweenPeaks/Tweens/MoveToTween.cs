ae7b4bc68cd53ca4a1f3c01f9a274bbc381f2674
Oleg Andrushko <nubick@gmail.com>
2018-03-29 23:57:11 +0700

    MoveToTween: Updated to use with RectTransform.anchoredPosition.

--- TweenPeaks/Assets/Scripts/Utils/TweenPeaks/Tweens/MoveToTween.cs
+++ TweenPeaks/Assets/Scripts/Utils/TweenPeaks/Tweens/MoveToTween.cs
@@ -7,9 +7,12 @@ namespace Assets.Scripts.Utils.TweenPeaks.Tweens
         private Vector3 _startPosition;
         private Vector3 _targetPosition;
         private bool _isLocal;
-	    private float? _targetX;
-	    private float? _targetY;
-	    private float? _targetZ;
+        private float? _targetX;
+        private float? _targetY;
+        private float? _targetZ;
+
+        private bool _isAnchoredPosition;
+        private RectTransform _rectTransform;
 
         public static MoveToTween Run(GameObject item, Vector3 position, float duration)
         {
@@ -18,44 +21,64 @@ namespace Assets.Scripts.Utils.TweenPeaks.Tweens
             return tween;
         }
 
-		public static MoveToTween RunX(GameObject item, float x, float duration)
-		{
-			MoveToTween tween = Create<MoveToTween>(item, duration);
-			tween._targetX = x;
-			return tween;
-		}
+        public static MoveToTween Run(RectTransform rectTransform, Vector3 anchoredPosition, float duration)
+        {
+            MoveToTween tween = Create<MoveToTween>(rectTransform.gameObject, duration);
+            tween._isAnchoredPosition = true;
+            tween._targetPosition = anchoredPosition;
+            tween._rectTransform = rectTransform;
+            return tween;
+        }
 
-		public static MoveToTween RunY(GameObject item, float y, float duration)
-		{
-			MoveToTween tween = Create<MoveToTween>(item, duration);
-			tween._targetY = y;
-			return tween;
-		}
+        public static MoveToTween RunX(GameObject item, float x, float duration)
+        {
+            MoveToTween tween = Create<MoveToTween>(item, duration);
+            tween._targetX = x;
+            return tween;
+        }
 
-		public static MoveToTween RunZ(GameObject item, float z, float duration)
-		{
-			MoveToTween tween = Create<MoveToTween>(item, duration);
-			tween._targetZ = z;
-			return tween;
-		}
-		
+        public static MoveToTween RunY(GameObject item, float y, float duration)
+        {
+            MoveToTween tween = Create<MoveToTween>(item, duration);
+            tween._targetY = y;
+            return tween;
+        }
+
+        public static MoveToTween RunZ(GameObject item, float z, float duration)
+        {
+            MoveToTween tween = Create<MoveToTween>(item, duration);
+            tween._targetZ = z;
+            return tween;
+        }
+
         public MoveToTween SetLocal(bool isLocal)
         {
             _isLocal = isLocal;
             return this;
         }
 
-	    protected override void OnStart()
-	    {
-		    _startPosition = _isLocal ? transform.localPosition : transform.position;
+        protected override void OnStart()
+        {
+            _startPosition = GetStartPosition();
 
-		    if (_targetX.HasValue)
-			    _targetPosition = new Vector3(_targetX.Value, _startPosition.y, _startPosition.z);
-		    else if (_targetY.HasValue)
-			    _targetPosition = new Vector3(_startPosition.x, _targetY.Value, _startPosition.z);
-		    else if (_targetZ.HasValue)
-			    _targetPosition = new Vector3(_startPosition.x, _startPosition.y, _targetZ.Value);
-	    }
+            if (_targetX.HasValue)
+                _targetPosition = new Vector3(_targetX.Value, _startPosition.y, _startPosition.z);
+            else if (_targetY.HasValue)
+                _targetPosition = new Vector3(_startPosition.x, _targetY.Value, _startPosition.z);
+            else if (_targetZ.HasValue)
+                _targetPosition = new Vector3(_startPosition.x, _startPosition.y, _targetZ.Value);
+        }
+
+        private Vector3 GetStartPosition()
+        {
+            if (_isAnchoredPosition)
+                return _rectTransform.anchoredPosition;
+
+            if (_isLocal)
+                return transform.localPosition;
+
+            return transform.position;
+        }
 
         protected override void UpdateValue(float time)
         {
@@ -64,10 +87,17 @@ namespace Assets.Scripts.Utils.TweenPeaks.Tweens
                 EaseFunc(_startPosition.y, _targetPosition.y, time),
                 EaseFunc(_startPosition.z, _targetPosition.z, time));
 
-            if (_isLocal)
-                transform.localPosition = nextPosition;
+            if (_isAnchoredPosition)
+            {
+                _rectTransform.anchoredPosition = nextPosition;
+            }
             else
-                transform.position = nextPosition;
+            {
+                if (_isLocal)
+                    transform.localPosition = nextPosition;
+                else
+                    transform.position = nextPosition;
+            }
         }
     }
 }
