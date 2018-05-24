4a9f170551aa0363cb543677a42adbbbaf68b2f3
Oleg Andrushko <nubick@gmail.com>
2018-05-24 21:30:49 +0700

    ScaleTween: RunX, RunY and RunZ new methods.

--- TweenPeaks/Assets/Plugins/TweenPeaks/Tweens/ScaleTween.cs
+++ TweenPeaks/Assets/Plugins/TweenPeaks/Tweens/ScaleTween.cs
@@ -2,34 +2,64 @@
 
 namespace TweenPeaks.Tweens
 {
-    public class ScaleTween : TweenBase
-    {
-        private Vector3 _startScale;
-        private Vector3 _targetScale;
+	public class ScaleTween : TweenBase
+	{
+		private Vector3 _startScale;
+		private Vector3 _targetScale;
+		private bool[] _scalingCoordinates;
 
-        public static ScaleTween Run(GameObject item, Vector3 scale, float duration)
-        {
-            ScaleTween tween = Create<ScaleTween>(item, duration);
-            tween._targetScale = scale;
-            return tween;
-        }
+		public static ScaleTween Run(GameObject item, Vector3 scale, float duration)
+		{
+			ScaleTween tween = Create<ScaleTween>(item, duration);
+			tween._targetScale = scale;
+			tween._scalingCoordinates = new bool[] { true, true, true };
+			return tween;
+		}
 
-        public static ScaleTween Run(GameObject item, float scale, float duration)
-        {
-            return Run(item, Vector3.one * scale, duration);
-        }
+		public static ScaleTween Run(GameObject item, float scale, float duration)
+		{
+			return Run(item, Vector3.one * scale, duration);
+		}
 
-        protected override void OnStart()
-        {
-            _startScale = transform.localScale;
-        }
+		public static ScaleTween RunX(GameObject item, float scaleX, float duration)
+		{
+			ScaleTween tween = Create<ScaleTween>(item, duration);
+			tween._targetScale = new Vector3(scaleX, 0f, 0f);
+			tween._scalingCoordinates = new bool[] { true, false, false };
+			return tween;
+		}
 
-        protected override void UpdateValue(float time)
-        {
-            transform.localScale = new Vector3(
-                EaseFunc(_startScale.x, _targetScale.x, time),
-                EaseFunc(_startScale.y, _targetScale.y, time),
-                EaseFunc(_startScale.z, _targetScale.z, time));
-        }
-    }
+		public static ScaleTween RunY(GameObject item, float scaleY, float duration)
+		{
+			ScaleTween tween = Create<ScaleTween>(item, duration);
+			tween._targetScale = new Vector3(0f, scaleY, 0f);
+			tween._scalingCoordinates = new bool[] { false, true, false };
+			return tween;
+		}
+
+		public static ScaleTween RunZ(GameObject item, float scaleZ, float duration)
+		{
+			ScaleTween tween = Create<ScaleTween>(item, duration);
+			tween._targetScale = new Vector3(0f, 0f, scaleZ);
+			tween._scalingCoordinates = new bool[] { false, false, true };
+			return tween;
+		}
+
+		protected override void OnStart()
+		{
+			_startScale = transform.localScale;
+			_targetScale = new Vector3(
+				_scalingCoordinates[0] ? _targetScale.x : _startScale.x,
+				_scalingCoordinates[1] ? _targetScale.y : _startScale.y,
+				_scalingCoordinates[2] ? _targetScale.z : _startScale.z);
+		}
+
+		protected override void UpdateValue(float time)
+		{
+			transform.localScale = new Vector3(
+				EaseFunc(_startScale.x, _targetScale.x, time),
+				EaseFunc(_startScale.y, _targetScale.y, time),
+				EaseFunc(_startScale.z, _targetScale.z, time));
+		}
+	}
 }
--- TweenPeaks/Assets/Scripts/Demo/TweenActions/ScaleTweenAction.cs
+++ TweenPeaks/Assets/Scripts/Demo/TweenActions/ScaleTweenAction.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 
 namespace Assets.Scripts.Demo.TweenActions
 {
-    public class ScaleTweenAction : TweenAction
-    {
-        public override void Run(GameObject target, TweenActionSettings s)
-        {
-            TweenSequence.Run2(
-                () => ScaleTween.Run(target, Vector3.zero, s.Duration).SetEase(s.Ease).SetDelay(s.Delay),
-                () => ScaleTween.Run(target, Vector3.one, s.Duration).SetEase(s.Ease));
-        }
-    }
+	public class ScaleTweenAction : TweenAction
+	{
+		public override void Run(GameObject target, TweenActionSettings s)
+		{
+			int variant = Random.Range(0, 3);
+
+			if (variant == 0)
+			{
+				TweenSequence.Run2(
+					() => ScaleTween.Run(target, Vector3.zero, s.Duration).SetEase(s.Ease).SetDelay(s.Delay),
+					() => ScaleTween.Run(target, Vector3.one, s.Duration).SetEase(s.Ease));
+			}
+			else if (variant == 1)
+			{
+				TweenSequence.Run2(
+					() => ScaleTween.RunX(target, 0f, s.Duration).SetEase(s.Ease).SetDelay(s.Delay),
+					() => ScaleTween.RunX(target, 1f, s.Duration).SetEase(s.Ease));
+			}
+			else if (variant == 2)
+			{
+				TweenSequence.Run2(
+					() => ScaleTween.RunY(target, 0f, s.Duration).SetEase(s.Ease).SetDelay(s.Delay),
+					() => ScaleTween.RunY(target, 1f, s.Duration).SetEase(s.Ease));
+			}
+		}
+	}
 }
